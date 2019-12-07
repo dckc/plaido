5d35b526a58c10e1b14dba195f4d748d0fc6ef2d
Dan Connolly <dckc@madmode.com>
2019-12-07 14:45:00 -0600

    localStorage config UI checkpoint:
    internalRequire:16 Uncaught ReferenceError: require is not defined
    at internalRequire:16

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const plaid = require('plaid');
+// ISSUE: I'd rather do this bootstrap in index.html
+/* global require */
+/* global document, localStorage */
+const UI = require('./ui');
+
+UI.startUI({
+  getForm: id => document.getElementById(id),
+  store: localStorage,
+});
---
+++ ui.js
@@ -0,0 +1,30 @@
+// const plaid = require('plaid');
+
+// const plaidClient = new plaid.Client(client_id, secret, public_key, plaid_env, {version: '2018-05-22'});
+/* global exports */
+
+// ISSUE: does FormData get all its authority from the form argument? or does it have some of its own?
+/* global FormData */
+exports = Object.freeze({ startUI });
+
+function startUI({ getForm, store }) {
+  const form = getForm('config');
+
+  load();
+  form.addEventListener('submit', save);
+
+  function load() {
+    const data = new FormData(form);
+    for (const k of data.keys()) {
+      form.elements[k].value = store.getItem(k);
+    }
+  }
+
+  function save(event) {
+    const data = new FormData(form);
+    for (const k of data.keys()) {
+      store.setItem(k, data.get(k));
+    }
+    event.preventDefault();
+  }
+}
